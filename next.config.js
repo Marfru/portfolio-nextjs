25640214d4d15e6d2ef1c011d27dba85c74bbb27
marfru <hola@marfru.com>
2020-09-01 08:48:50 +0200

    config

--- next.config.js
+++ next.config.js
@@ -17,7 +17,6 @@ module.exports = withCSS(withSass({
     distDir: 'build',
     target: 'serverless',
     env: {
-      // Will be available on both server and client
       CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
       CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN
     }
