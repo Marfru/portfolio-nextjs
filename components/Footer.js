b1fd6378ac2c8a8b0493792db6aad1434783a5bf
marfru <hola@marfru.com>
2020-09-02 23:34:09 +0200

    contentful

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
       <SkillSet/>
       <div className="footer">
         <p className="footer__content">
-          Made from scratch with <span style={{paddingRight: '7px'}} aria-label="fire" role="img">🔥</span> using React Hooks, <a href="https://nextjs.org/" rel="noreferrer nooponer" target="_blank">NextJS</a> and <a href="https://www.contentful.com/" rel="noreferrer nooponer" target="_blank">Contentful</a></p>
+          Made from scratch with <span style={{paddingRight: '7px'}} aria-label="fire" role="img">🔥</span> using React Hooks and <a href="https://nextjs.org/" rel="noreferrer nooponer" target="_blank">NextJS</a></p>
       </div>
     </div>
   )
